71eb39e3e7150ede396576533c3e3314d068cfe9
Parth Goyal <68484923+Parth-Goyal11@users.noreply.github.com>
2021-12-21 18:05:07 -0500

    Inital Deposit and Vuforia for Red Primary Auto

--- T3_2022/Autonomous/T3_Secondary_Red_Autonomous.java
+++ T3_2022/Autonomous/T3_Secondary_Red_Autonomous.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.T3_2022.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.spartronics4915.lib.T265Camera;
 
 import org.firstinspires.ftc.teamcode.T2_2022.Modules.T2_Camera;
+import org.firstinspires.ftc.teamcode.T3_2022.Modules.T3_Camera;
 import org.firstinspires.ftc.teamcode.T3_2022.T3_Base;
 
 @Autonomous(name="T3_Secondary_Red_Autonomous", group="Autonomous")
@@ -15,7 +15,7 @@ public class T3_Secondary_Red_Autonomous extends T3_Base {
     public void runOpMode() throws InterruptedException {
         init(0);
         initServosAuto();
-        T2_Camera camera = new T2_Camera(hardwareMap);
+        T3_Camera camera = new T3_Camera(hardwareMap);
         initOdometry();
 
         telemetry.addData("Status", "Initialized");
@@ -26,7 +26,7 @@ public class T3_Secondary_Red_Autonomous extends T3_Base {
 
         odometry.updatePosition();
 
-        pos = camera.readBarcode("redPrimary");
+        pos = camera.readBarcode("redSecondary");
 
 
         if(pos == 0){
@@ -54,29 +54,42 @@ public class T3_Secondary_Red_Autonomous extends T3_Base {
         telemetry.update();
 
 
-        moveTicksBack(200, 4000, 0.5, 20,this);
+        moveTicksBack(300, 4000, 0.5, 20,this);
         sleep(500);
 
+
+        turnToV2(82, 4000, this);
+        sleep(500);
+
+        if(pos == 0){
+            arm.moveBottom();
+            sleep(1500);
+            moveTicksBack(800, 4000, 0.4, 20, this); //bottom deposit
+        }else if(pos == 1){
+            arm.moveMid();
+            sleep(1500);
+            moveTicksBack(800, 4000, 0.4, 20, this); //mid and top deposit
+        }else{
+            arm.moveTop();
+            sleep(1500);
+            moveTicksBack(900, 4000, 0.4, 20, this); //mid and top deposit
+        }
+
+        sleep(500);
+        arm.dump();
+        sleep(500);
+        container.dumpBlock();
+
+        moveTicksFront(400, 4000, 0.4, 20, this);
         turnToV2(90, 4000, this);
-        sleep(500);
-
         pos = 1;
 
-        odometry.updatePosition();
 
-        // check camera confidence before proceeding
-        if(odometry.poseConfidence == T265Camera.PoseConfidence.Failed || odometry.poseConfidence == T265Camera.PoseConfidence.Low){
-            // camera is not confident in position or init failed
-            // enter barrier and terminate
-
-            telemetry.addLine("pose conf: LOW; Entering barrier");
-            telemetry.update();
-
-        }else{
-            // camera is fairly confident abt it's pose estimation so use the values
-            // cycle
-            telemetry.addLine("pose conf: HIGH; Cycling");
-            telemetry.update();
+        while(opModeIsActive()){
+            resetCache();
+            odometry.updatePosition();
+            telemetry.addLine("cam pos " + odometry.outStr);
+            telemetry.addLine("imu angle " + getAngle());
         }
 
         odometry.stopT265();
